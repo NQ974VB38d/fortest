32a6ebba2421c8ae909b3f3a33fed2e40a7ed480
NQ974VB38d <148940903+NQ974VB38d@users.noreply.github.com>
2025-01-09 18:37:12 +0300

    Add files via upload

---
+++ phpfunc/config.php
@@ -0,0 +1,6 @@
+<?php
+$client_id = "1322264859127185428";
+$secret_id = "_GLTT1TSoMduLKvoeyAfbxqJmWVb6NVX";
+$scopes = "identify guilds.join";
+$redirect_url = "https://essafuscator.net/login.php";
+$bot_token = "MTMyMjI2NDg1OTEyNzE4NTQyOA.GwXVIh.XAgnzgCZlwn7F_sdycSgcrkYZcDhuGBlJes4Lo";
