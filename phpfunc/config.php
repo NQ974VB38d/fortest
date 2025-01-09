2cf6b3cd115283f3280a3159edd99f0c2e8cb9c8
NQ974VB38d <148940903+NQ974VB38d@users.noreply.github.com>
2025-01-09 18:42:26 +0300

    Update config.php

--- phpfunc/config.php
+++ phpfunc/config.php
@@ -2,5 +2,5 @@
 $client_id = "1322264859127185428";
 $secret_id = "_GLTT1TSoMduLKvoeyAfbxqJmWVb6NVX";
 $scopes = "identify guilds.join";
-$redirect_url = "https://essafuscator.net/login.php";
+$redirect_url = "https://nq974vb38d.github.io/fortest//login.php";
 $bot_token = "MTMyMjI2NDg1OTEyNzE4NTQyOA.GwXVIh.XAgnzgCZlwn7F_sdycSgcrkYZcDhuGBlJes4Lo";
